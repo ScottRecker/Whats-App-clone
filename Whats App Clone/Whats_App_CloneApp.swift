4b85fba2a46a9cc2d6aca9e8f89388dcf69121ae
Scott Recker <scott.a.recker@gmail.com>
2024-05-23 23:25:30 -0500

    Add Xcode project

---
+++ Whats App Clone/Whats_App_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whats_App_CloneApp.swift
+//  Whats App Clone
+//
+//  Created by Scott Recker on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Whats_App_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
