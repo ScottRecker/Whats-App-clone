5d0a930b7e44b280c836fe1fc5c8af0a0abebba6
Scott Recker <scott.a.recker@gmail.com>
2024-05-25 12:36:45 -0500

    Rename view

--- Whats App Clone/Screens/Updates/Views/UpdatesTabScreen.swift
+++ Whats App Clone/Screens/Updates/Views/UpdatesTabScreen.swift
@@ -158,7 +158,7 @@ private struct ChannelListView: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack {
                     ForEach(0..<5) { _ in
-                        ChannelItemView()
+                        SuggestedChannelItemView()
                     }
                 }
             }
@@ -175,7 +175,7 @@ private struct ChannelListView: View {
     }
 }
 
-private struct ChannelItemView: View {
+private struct SuggestedChannelItemView: View {
     var body: some View {
         VStack {
             Circle()
