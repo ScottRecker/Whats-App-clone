6c8d1983454509c6cbc1e0fb61a7e4552e034947
Scott Recker <scott.a.recker@gmail.com>
2024-05-25 12:37:06 -0500

    Add Channel tab screen

---
+++ Whats App Clone/Screens/Channel/Views/ChannelItemView.swift
@@ -0,0 +1,44 @@
+//
+//  ChannelItemView.swift
+//  Whats App Clone
+//
+//  Created by Scott Recker on 5/25/24.
+//
+
+import SwiftUI
+
+struct ChannelItemView: View {
+    var body: some View {
+        HStack(alignment: .top, spacing: 10) {
+            Circle()
+                .frame(width: 60, height: 60)
+            VStack(alignment: .leading, spacing: 3) {
+                titleTextView()
+                lastMessagePreview()
+            }
+        }
+    }
+
+    private func titleTextView() -> some View {
+        HStack {
+            Text("Username here")
+                .lineLimit(1)
+                .bold()
+            Spacer()
+            Text("5:50 PM")
+                .foregroundStyle(.gray)
+                .font(.system(size: 15))
+        }
+    }
+
+    private func lastMessagePreview() -> some View {
+        Text("Hey welcome")
+            .font(.system(size: 16))
+            .lineLimit(2)
+            .foregroundStyle(.gray)
+    }
+}
+
+#Preview {
+    ChannelItemView()
+}
---
+++ Whats App Clone/Screens/Channel/Views/ChannelTabScreen.swift
@@ -0,0 +1,112 @@
+//
+//  ChannelTabScreen.swift
+//  Whats App Clone
+//
+//  Created by Scott Recker on 5/25/24.
+//
+
+import SwiftUI
+
+struct ChannelTabScreen: View {
+    @State private var searchText = ""
+    var body: some View {
+        NavigationStack {
+            List {
+                archivedButton()
+                ForEach(0..<3) { _ in
+                    ChannelItemView()
+                }
+                inboxFooterView()
+                    .listRowSeparator(.hidden)
+            }
+            .navigationTitle("Chats")
+            .searchable(text: $searchText)
+            .listStyle(.plain)
+            .toolbar {
+                leadingNavItem()
+                trailingNavItem()
+            }
+        }
+    }
+}
+
+extension ChannelTabScreen {
+    @ToolbarContentBuilder
+    private func leadingNavItem() -> some ToolbarContent {
+        ToolbarItem(placement: .topBarLeading) {
+            Menu {
+                Button {
+                    print("menu button tapped")
+                } label: {
+                    Label("Select Chats", systemImage: "checkmark.circle")
+                }
+
+            } label: {
+                Image(systemName: "ellipsis.circle")
+            }
+        }
+    }
+
+    @ToolbarContentBuilder
+    private func trailingNavItem() -> some ToolbarContent {
+        ToolbarItemGroup(placement: .topBarTrailing) {
+            aiButton()
+            cameraButton()
+            newChatButton()
+        }
+    }
+
+    private func aiButton() -> some View {
+        Button {
+
+        } label: {
+            Image(.circle)
+        }
+    }
+
+    private func newChatButton() -> some View {
+        Button {
+
+        } label: {
+            Image(.plus)
+        }
+    }
+
+    private func cameraButton() -> some View {
+        Button {
+
+        } label: {
+            Image(systemName: "camera")
+        }
+    }
+
+    private func archivedButton() -> some View {
+        Button {
+
+        } label: {
+            Label("Archived", systemImage: "archivebox.fill")
+                .bold()
+                .padding()
+                .foregroundStyle(.gray)
+        }
+    }
+
+    private func inboxFooterView() -> some View {
+        HStack {
+            Image(systemName: "lock.fill")
+            (
+                Text("Your personal messages are ")
+                +
+                Text("end-to-end encrypted")
+                    .foregroundColor(.blue)
+            )
+        }
+        .foregroundColor(.gray)
+        .font(.caption)
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    ChannelTabScreen()
+}
