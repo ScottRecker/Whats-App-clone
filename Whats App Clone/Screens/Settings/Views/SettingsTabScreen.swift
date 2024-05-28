c3cf9a29d79e323ff27684c29a92173788656023
Scott Recker <scott.a.recker@gmail.com>
2024-05-28 16:41:58 -0500

    Add Sign out button to settings screen

--- Whats App Clone/Screens/Settings/Views/SettingsTabScreen.swift
+++ Whats App Clone/Screens/Settings/Views/SettingsTabScreen.swift
@@ -31,6 +31,21 @@ struct SettingsTabScreen: View {
             }
             .navigationTitle("Settings")
             .searchable(text: $searchText)
+            .toolbar {
+                leadingNavItem()
+            }
+        }
+    }
+}
+
+extension SettingsTabScreen {
+    @ToolbarContentBuilder
+    private func leadingNavItem() -> some ToolbarContent {
+        ToolbarItem(placement: .topBarLeading) {
+            Button("Sign Out") {
+                Task { try? await AuthManager.shared.logOut() }
+            }
+            .foregroundStyle(.red)
         }
     }
 }
