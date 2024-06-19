80f917185d82032b73d34189296eeeeeb12493a8
Scott Recker <scott.a.recker@gmail.com>
2024-06-19 14:42:21 -0500

    Sort messages by time

--- Whats App Clone/Services/MessageService.swift
+++ Whats App Clone/Services/MessageService.swift
@@ -40,6 +40,9 @@ struct MessageService {
                 let messageDict = value as? [String: Any] ?? [:]
                 let message = MessageItem(id: key, dict: messageDict)
                 messages.append(message)
+                if messages.count == snapshot.childrenCount {
+                    messages.sort { $0.timeStamp < $1.timeStamp }
+                }
                 completion(messages)
             }
         } withCancel: { error in
