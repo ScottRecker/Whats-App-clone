c251753af49d17402bd776dc904da854e97d3f00
Scott Recker <scott.a.recker@gmail.com>
2024-06-19 20:55:33 -0500

    Add circular profile image views

--- Whats App Clone/Screens/Channel/Model/ChannelItem.swift
+++ Whats App Clone/Screens/Channel/Model/ChannelItem.swift
@@ -19,11 +19,23 @@ struct ChannelItem: Identifiable {
     var adminUids: [String]
     var membersUids: [String]
     var members: [UserItem]
-    var thumbnailUrl: String?
+    private var thumbnailUrl: String?
     var createdBy: String
 
     var isGroupChat: Bool {
         membersCount > 2
+    }
+
+    var coverImageUrl: String? {
+        if let thumbnailUrl = thumbnailUrl {
+            return thumbnailUrl
+        }
+
+        if isGroupChat == false {
+            return membersExcludingMe.first?.profileImageUrl
+        }
+
+        return nil
     }
 
     var membersExcludingMe: [UserItem] {
